26b5654d7b6f03b81a874a861b36685f76504119
grant <grant.lubeck@gmail.com>
2024-03-30 15:12:47 -0400

    initial gruvbox css commit.

--- cmd/template.go
+++ cmd/template.go
@@ -81,7 +81,7 @@ func StatusTemplate(store ServiceStore, w http.ResponseWriter) {
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href="/themes/classic.css">
+    <link rel = "stylesheet" href="/themes/gruvbox.css">
     <title>Service Status</title>
     </head>
     <body>
